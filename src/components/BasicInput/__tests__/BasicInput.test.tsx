3be07882a3c7e2ad30ae38a68ff6a17d593fc40a
Anton Kuchmii <a.kuchmiy19@gmail.com>
2022-01-15 14:13:26 +0200

    BasicInput component tests

---
+++ src/components/BasicInput/__tests__/BasicInput.test.tsx
@@ -0,0 +1,37 @@
+import { shallow } from 'enzyme'
+import BasicInput from '../BasicInput'
+
+describe('BasicInput tests', () => {
+  it('should render input with text type', function () {
+    const input = shallow(<BasicInput />)
+
+    expect(input.containsMatchingElement(<input type="text" />)).toBe(true)
+  })
+
+  it('should render input with different type', function () {
+    const input = shallow(<BasicInput type={'number'} />)
+
+    expect(input.containsMatchingElement(<input type="number" />)).toBe(true)
+  })
+
+  it('should pass rest props to the input', function () {
+    const input = shallow(
+      <BasicInput aria-label={'foo'} aria-controls={'bar'} />
+    )
+
+    expect(
+      input.containsMatchingElement(
+        <input aria-label={'foo'} aria-controls={'bar'} />
+      )
+    ).toBe(true)
+  })
+
+  it('should call onChange function', () => {
+    const onChange = jest.fn()
+    const input = shallow(<BasicInput onChange={onChange} />)
+
+    input.find('input').simulate('change')
+
+    expect(onChange).toBeCalledTimes(1)
+  })
+})

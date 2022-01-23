7f4d57855de6a491edfb25d6fc0711fbe984d97f
Anton Kuchmii <a.kuchmiy19@gmail.com>
2022-01-23 19:35:58 +0200

    useFeed env usage

--- src/entities/Feed/model/index.ts
+++ src/entities/Feed/model/index.ts
@@ -1,14 +1,14 @@
 import { useCallback } from 'react'
 import { Feed, getHashtagFeed, getTrendingFeed, getUserFeed } from 'shared/api'
 import { useAsync } from 'shared/hooks/useAsync'
+import { isProdEnv } from 'shared/config'
 
 export function useFeed(
   username: string | undefined,
   queryParam: string | null
 ) {
   const fetchFeed = useCallback(() => {
-    if (username && process.env.NODE_ENV === 'production')
-      return getUserFeed(username)
+    if (username && isProdEnv) return getUserFeed(username)
     if (queryParam) return getHashtagFeed(queryParam)
 
     return getTrendingFeed()
